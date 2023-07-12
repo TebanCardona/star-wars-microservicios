3e19dbd4eb8b5601193a0b6c435637367641fc91
TebanCardona <estebanfabi737@gmail.com>
2023-07-12 15:45:58 -0500

    test

--- databases/src/databases/index.js
+++ databases/src/databases/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("../config/envs");
+console.log(MONGO_URI);
 const schemas = require("./schemas");
 const conn = mongoose.createConnection(MONGO_URI);
 module.exports = {
